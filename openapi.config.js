be396cded310da94ae97fa3a8116b6598aa367f9
t1 <1w@qq.com>
2025-01-26 15:46:06 +0800

    Revert "用户传图-审核和url前端实现"
    This reverts commit a3555ad71567184bfe05d3ee04be7f90d69f2e1f.

--- openapi.config.js
+++
@@ -1,9 +0,0 @@
-import { generateService } from '@umijs/openapi'
-
-// openapi配置文件
-generateService({
-  requestLibPath: "import request from '@/request'",
-  // 接口文档地址
-  schemaPath: 'http://localhost:8123/api/v2/api-docs',
-  serversPath: './src',
-})
